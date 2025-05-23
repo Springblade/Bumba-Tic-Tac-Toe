6ea84644e35e78a2dd09737ae9bee155154ce76d
Tuan-NTA <135790241+Tuan-NTA@users.noreply.github.com>
2025-05-23 14:02:46 +0700

    FIXES

--- src/main/java/Tic_Tac_Toe/common/message.java
+++
@@ -1,7 +0,0 @@
-package Tic_Tac_Toe.common;
-
-public interface message {
-    String getType();
-    String getGameId();
-    String getContent();
-}
--- src/main/java/Tic_Tac_Toe/server/gameController.java
+++
@@ -1,8 +0,0 @@
-package Tic_Tac_Toe.server;
-
-import Tic_Tac_Toe.game.*;
-
-public class gameController implements Runnable {
-
-
-}
